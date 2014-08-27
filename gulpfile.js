822b2d3ecb6158d331d3d918d12e8aecb6823b52
Hongru Hou <i@houhongru.com>
2014-08-27 07:54:36 -0700

    New images folder tree

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,28 @@
 var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
-    autoPrefixer = require('gulp-autoprefixer'),
+    autoprefixer = require('gulp-autoprefixer'),
     notify = require('gulp-notify'),
-    minifyCSS = require('gulp-minify-css'),
-    rename = require('gulp-rename');
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    imagemin = require('gulp-imagemin');
 
 gulp.task('css', function() {
   return gulp.src('scss/main.scss')
     .pipe(sass({ style: 'expanded' }))
     .on('error', notify.onError("<%= error.message %>"))
-    .pipe(autoPrefixer('last 2 version', '> 5%'))
+    .pipe(autoprefixer('last 2 version', '> 5%'))
     .pipe(gulp.dest('css'))
     .pipe(rename({suffix: '.min'}))
-    .pipe(minifyCSS())
+    .pipe(minifycss())
     .pipe(gulp.dest('css'))
     .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('images', function() {
+  return gulp.src('images/offline/**/*', {base: './images/offline/'})
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('images/online'))
+    .pipe(notify({ message: 'Images task complete' }));
 });
 
 // Watch
